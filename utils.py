ee8d9be2fbd3496e1089af58436d867f9597f114
Andrew Mussey <git@amussey.com>
2016-07-06 18:00:36 -0700

    Remove events that have been deleted.

--- utils.py
+++ utils.py
@@ -69,43 +69,44 @@ def groupme_json_to_ics(groupme_json, static_name=None):
     cal['x-wr-timezone'] = 'America/Los_Angeles'
 
     for json_blob in groupme_json['response']['events']:
-        event = Event()
-        event['uid'] = json_blob['event_id']
-        event.add('dtstart', dateutil.parser.parse(json_blob['start_at']))
-        if json_blob.get('end_at'):
-            event.add('dtend', dateutil.parser.parse(json_blob['end_at']))
-        event['summary'] = json_blob['name']
-        event['description'] = json_blob.get('description', '')
-        if json_blob.get('location'):
-            location = json_blob.get('location', {})
+        if 'deleted_at' not in json_blob:
+            event = Event()
+            event['uid'] = json_blob['event_id']
+            event.add('dtstart', dateutil.parser.parse(json_blob['start_at']))
+            if json_blob.get('end_at'):
+                event.add('dtend', dateutil.parser.parse(json_blob['end_at']))
+            event['summary'] = json_blob['name']
+            event['description'] = json_blob.get('description', '')
+            if json_blob.get('location'):
+                location = json_blob.get('location', {})
 
-            if json_blob.get('description'):
-                event['description'] += '\n\n'
-            event['description'] += 'Location:\n'
+                if json_blob.get('description'):
+                    event['description'] += '\n\n'
+                event['description'] += 'Location:\n'
 
-            if location.get('name') and location.get('address'):
-                event['location'] = "{}, {}".format(location.get('name'), location.get('address').strip().replace("\n", ", "))
-                event['description'] += location.get('name')
-                event['description'] += '\n'
-                event['description'] += location.get('address')
-            elif location.get('name'):
-                event['location'] = location.get('name')
-                event['description'] += location.get('name')
-            elif location.get('address'):
-                event['location'] = location.get('address').strip().replace("\n", ", ")
-                event['description'] += location.get('address')
-
-            if location.get('lat') and location.get('lng'):
-                location_url = 'https://www.google.com/maps?q={},{}'.format(location.get('lat'), location.get('lng'))
-                if not event.get('location'):
-                    event['location'] = location_url
-                else:
+                if location.get('name') and location.get('address'):
+                    event['location'] = "{}, {}".format(location.get('name'), location.get('address').strip().replace("\n", ", "))
+                    event['description'] += location.get('name')
                     event['description'] += '\n'
-                event['description'] += location_url
+                    event['description'] += location.get('address')
+                elif location.get('name'):
+                    event['location'] = location.get('name')
+                    event['description'] += location.get('name')
+                elif location.get('address'):
+                    event['location'] = location.get('address').strip().replace("\n", ", ")
+                    event['description'] += location.get('address')
 
-        if json_blob.get('updated_at'):
-            event['last-modified'] = dateutil.parser.parse(json_blob.get('updated_at'))
-        cal.add_component(event)
+                if location.get('lat') and location.get('lng'):
+                    location_url = 'https://www.google.com/maps?q={},{}'.format(location.get('lat'), location.get('lng'))
+                    if not event.get('location'):
+                        event['location'] = location_url
+                    else:
+                        event['description'] += '\n'
+                    event['description'] += location_url
+
+            if json_blob.get('updated_at'):
+                event['last-modified'] = dateutil.parser.parse(json_blob.get('updated_at'))
+            cal.add_component(event)
 
     return cal.to_ical()
 
